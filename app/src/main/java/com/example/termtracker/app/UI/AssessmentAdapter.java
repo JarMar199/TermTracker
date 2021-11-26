d0c13c995958fd521b6a34f9e60e145629559abf
Jarra Maratea <JarMar199@gmail.com>
2021-11-26 14:17:47 -0800

    Assessment Adapter, List Item, Get Course Info for Assessment List

---
+++ app/src/main/java/com/example/termtracker/app/UI/AssessmentAdapter.java
@@ -0,0 +1,80 @@
+package com.example.termtracker.app.UI;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.termtracker.R;
+
+import java.util.List;
+
+import Entities.Assessment;
+
+public class AssessmentAdapter extends RecyclerView.Adapter<AssessmentAdapter.AssessmentViewHolder> {
+    class AssessmentViewHolder extends RecyclerView.ViewHolder {
+        private final TextView assessmentItemView;
+
+        private AssessmentViewHolder(View itemView) {
+            super(itemView);
+            assessmentItemView = itemView.findViewById(R.id.assessmentTextView);
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    final Assessment current = mAssessment.get(position);
+
+                    Intent intent = new Intent(context, AssessmentDetail.class);
+                    intent.putExtra("assessmentName", current.getAssessmentName());
+                    intent.putExtra("assessmentId", current.getAssessmentId());
+                    intent.putExtra("assessmentStart", current.getStartDate());
+                    intent.putExtra("assessmentEnd", current.getEndDate());
+                    intent.putExtra("assessmentCourseId", current.getCourseId());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
+    private List<Assessment> mAssessment;
+    private final Context context;
+    private final LayoutInflater mInflater;
+
+    public AssessmentAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public AssessmentAdapter.AssessmentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.assessment_list_item,parent,false);
+        return new AssessmentViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AssessmentViewHolder holder, int position) {
+        if (mAssessment!= null) {
+            final Assessment current = mAssessment.get(position);
+            holder.assessmentItemView.setText(current.getAssessmentName());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mAssessment != null)
+            return mAssessment.size();
+        else
+            return 0;
+    }
+
+    public void setAssessment(List<Assessment> assessments) {
+        mAssessment = assessments;
+        notifyDataSetChanged();
+    }
+}
--- app/src/main/java/com/example/termtracker/app/UI/AssessmentList.java
+++ app/src/main/java/com/example/termtracker/app/UI/AssessmentList.java
@@ -1,18 +1,44 @@
 package com.example.termtracker.app.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.termtracker.R;
 
-public class AssessmentList extends AppCompatActivity {
+import Database.Repository;
 
+public class AssessmentList extends AppCompatActivity {
+    String courseName, startDate, endDate, status, insName, insPhone, insEmail, note;
+    int courseId, termId;
+    TextView textName, textStart, textEnd, textStatus, textInsName, textInsPhone, textInsEmail, textNote;
+    Repository repository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_list);
+
+        courseName = getIntent().getStringExtra("courseName");
+        startDate = getIntent().getStringExtra("courseStart");
+        endDate = getIntent().getStringExtra("courseEnd");
+        status = getIntent().getStringExtra("courseStatus");
+        insName = getIntent().getStringExtra("courseInsName");
+        insPhone = getIntent().getStringExtra("courseInsPhone");
+        insEmail = getIntent().getStringExtra("courseInsEmail");
+        note = getIntent().getStringExtra("courseNote");
+        courseId = getIntent().getIntExtra("courseId", -1);
+        termId = getIntent().getIntExtra("courseTermId", -1);
+        repository = new Repository(getApplication());
+        repository.getAssociatedAssessments(courseId);
+        RecyclerView recyclerView = findViewById(R.id.assessmentRecyclerView);
+        final AssessmentAdapter adapter = new AssessmentAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter.setAssessment(repository.getAssociatedAssessments(courseId));
     }
 
     public void addAssessment(View view) {
